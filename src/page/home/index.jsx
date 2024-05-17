2085bdfdc538682c174d697c4ae6266bc28db2a1
PhongNguyen <phong9xtb@gmail.com>
2024-05-17 14:01:37 +0700

    update 17/5

--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { useEffect, useState } from 'react';
-import { Button } from 'antd';
+import { useState } from 'react';
+import { Button, message, Table  } from 'antd';
 
 const Home = () => {
-    const [lessionChoise, setLessionChoise] = useState()
+	const [messageApi, contextHolder] = message.useMessage();
+    const [lessonChoise, setLessonChoise] = useState()
 	const [wordRandom, setWordRandom] = useState('')
-
-	const listLession = [
+	const [dataLesson, setDataLesson] = useState('')
+	const [listLesson, setListLesson] = useState([
 		{
 			ID: 1,
-			Name: 'Lession 1',
+			Name: 'Lesson 1',
 			ListWord: [
 				{
 					ID: 1,
@@ -25,7 +26,7 @@ const Home = () => {
 		},
 		{
 			ID: 2,
-			Name: 'Lession 2',
+			Name: 'Lesson 2',
 			ListWord: [
 				{
 					ID: 1,
@@ -39,32 +40,86 @@ const Home = () => {
 				}
 			]
 		},
+		{
+			ID: 3,
+			Name: 'Lesson 3',
+			ListWord: [
+				{
+					ID: 1,
+					Word: 'home',
+					Mean: 'ngôi nhà'
+				},
+				{
+					ID: 2,
+					Word: 'address',
+					Mean: 'địa chỉ'
+				}
+			]
+		},
+	])
+
+	const columns = [
+		{
+			title: 'Từ vựng',
+			dataIndex: 'Word',
+			key: 'Word',
+		},
+		{
+			title: 'Nghĩa',
+			dataIndex: 'Mean',
+			key: 'Mean',
+		}
 	]
 
-	const changeLession = (item) => {
+	const changeLesson = (item) => {
 		console.log('item', item);
-		setLessionChoise(item)
+		setLessonChoise(item)
+		randomWord()
 	}
 	
 	const randomWord = () => {
-		var random = Math.floor(Math.random() * listLession[lessionChoise-1]?.ListWord?.length);
-		setWordRandom( listLession[lessionChoise-1]?.ListWord[random] )
+		var random = Math.floor(Math.random() * listLesson[lessonChoise-1]?.ListWord?.length);
+		setWordRandom( listLesson[lessonChoise-1]?.ListWord[random] )
+		console.log(wordRandom?.Word);
 	}
-	
-	useEffect(() => {
-		randomWord()
-	}, [lessionChoise])
+
+	const addLesson = () => {
+		if ( dataLesson.length !== 0 ) {
+			console.log('add lesson', dataLesson);
+			setDataLesson('')
+		} else {
+			messageApi.error('Không được để trống');
+		}
+	}
+
+	const inputLesson = (e) => {
+		setDataLesson(e.target.value)
+	}
+
 	return (
 		<div className='over__view'>
+			{contextHolder}
 			<div className='container'>
 				{
-					listLession.map((item) => {
+					listLesson.map((item) => {
 						return (
-							<Button onClick={() => { changeLession(item.ID) }} key={item.ID}>{item.Name}</Button>
+							<Button onClick={() => { changeLesson(item.ID) }} key={item.ID}>{item.Name}</Button>
 						)
 					})
 				}
 				{ wordRandom?.Word }
+				<input value={dataLesson}  onChange={inputLesson} />
+				<Button onClick={addLesson}>Add lesson</Button>
+				{
+					listLesson.map((item) => {
+						return (
+							< div key={item.ID}>
+								<div >{item.Name}</div>
+								<Table dataSource={item.ListWord} columns={columns} />
+							</div>
+						)
+					})
+				}
 			</div>
 		</div>
 	);
